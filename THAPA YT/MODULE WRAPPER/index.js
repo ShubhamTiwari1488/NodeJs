ba641d76fddb1fec7665ec22f01d70292f494db2
SHUBHAM TIWARI <73779227+shubham488-creator@users.noreply.github.com>
2023-05-20 21:16:02 +0530

    Add files via upload

---
+++ THAPA YT/MODULE WRAPPER/index.js
@@ -0,0 +1,15 @@
+/* Module Wrapper Function */
+
+// ( function (exports,require,module,__filename,__dirname){
+//     const name = "Shubham";
+//     console.log(name);
+// });
+
+
+// Inorder to run the Module Wrapper Function
+
+
+( function (exports,require,module,__filename,__dirname){
+    const name = "Shubham";
+    console.log(name);
+})();
